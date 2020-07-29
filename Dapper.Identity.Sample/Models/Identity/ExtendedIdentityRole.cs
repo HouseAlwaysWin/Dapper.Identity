a7379f0fda1eed0f24f5ce9e1d4e36c917a5dfab
MartinWang <martinwang7963@gmail.com>
2020-07-29 20:47:50 +0800

    Add sample

---
+++ Dapper.Identity.Sample/Models/Identity/ExtendedIdentityRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper.Identity.Sample.Models.Identity
+{
+    public class ExtendedIdentityRole : IdentityRole<string>
+    {
+        public string Description { get; set; }
+    }
+}
